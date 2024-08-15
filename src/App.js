68fde1edd96a2d9ce60964775ea7bc9160b9679e
hrithikmani <hrithik.mani2000@gmail.com>
2024-08-15 11:43:51 -0400

    Added a route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/documentation" element={<Documentation />} />
+          <Route path="/herbie" element={<HomePage />} />
         </Routes>
       </div>
     </Router>
